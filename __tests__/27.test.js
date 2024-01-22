c96d0c96c0e222e80c376d42523d30712d316725
Ash <mrAMFarrar@gmail.com>
2024-01-22 10:46:11 +0000

    code: 27, tests

--- __tests__/27.test.js
+++ __tests__/27.test.js
@@ -14,17 +14,45 @@ describe("Testing: removeElement function from 27.js", () => {
             
             expect(arr).toEqual([2,2])
         })
-        test("test 1: return value", () => {
+        test("test 2: return value", () => {
             const arr = [0,1,2,2,3,0,4,2]
 
             expect(removeElement(arr, 2)).toBe(5)
         })
-        test("test 1: array mutation", () => {
+        test("test 2: array mutation", () => {
             const arr = [0,1,2,2,3,0,4,2]
 
             removeElement(arr, 2)
             
             expect(arr).toEqual([0,1,3,0,4])
+        })
+    })
+    describe("aditional tests", () => {
+        test("returns a number", () => {
+            const arr = [0,1,2,2,3,0,4,2]
+
+            expect(typeof removeElement(arr, 2)).toEqual("number")
+        })
+        test("mutates array", () => {
+            const arr = [1,2,3]
+
+            removeElement(arr, 1)
+            
+            expect(arr).toEqual([2,3])
+        })
+        test("empty array returns 0", () => {
+            const arr = []
+
+            expect(removeElement(arr, 1)).toEqual(0)
+        })
+        test("doesnt mutate array, when given val not in array", () => {
+            const arr = [1,2,3]
+            const arrClone = [1,2,3]
+
+
+            removeElement(arr, 4)
+
+            expect(arr).toEqual(arrClone)
         })
     })
 })
