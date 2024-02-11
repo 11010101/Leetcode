94a1d1aa171978b608c42e71e7326c28922a2c74
11010101 <10110100@protonmail.com>
2024-02-11 17:42:24 +0000

    code: 14

---
+++ easy/14.js
@@ -0,0 +1,12 @@
+const longestCommonPrefix = (strs) => {
+    let prefix = strs[0]
+
+    for (let i = 1; i < strs.length; i++) {
+        for (let j = 0; j < prefix.length; j++) {
+            if (prefix[j] !== strs[i][j]) {
+                prefix = prefix.substring(0, j)
+            }
+        }
+    }
+    return prefix
+}
