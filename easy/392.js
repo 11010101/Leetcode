d0b02f601f55dbcbeb6ffb4b7da3053744d47563
11010101 <10110100@protonmail.com>
2024-02-16 17:11:36 +0000

    code: 392

---
+++ easy/392.js
@@ -0,0 +1,23 @@
+/*
+    beats 99.98%
+*/
+
+const isSubsequence = (s, t) => {
+    if (s.length === 0 || t.length === 0) {
+        return true
+    }
+    
+    let sIndex = 0
+    for (let tIndex = 0; tIndex < t.length; tIndex++) {
+
+        if (s[sIndex] === t[tIndex]) {
+            sIndex++
+        }
+
+        if (sIndex === s.length) {
+            return true
+        }
+
+    }
+    return false
+}
